fddc68a90183e6e3d3ef06772677ef3f5e285b7e
James Lopez <jlopez@myokapi.com>
2024-06-20 16:00:44 -0500

    Task 1 - Minimum Viable Quiz

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,11 +3,11 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = " ";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
+let question = "Who was the first American woman in space? ";
+let correctAnswer = "Sally Ride";
+let candidateAnswer = " ";
 
 
 //TODO: Variables for Part 2
@@ -18,19 +18,26 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-
+  console.log("Enter your" + candidateName);
 }
 
-function askQuestion() {
+function askQuestion(candidateAnswer) {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
-
+  console.log("Who was the first American woman in space?");
+  console.log("True or false: 5 kilometer == 5000 meters?"); 
+  console.log("(5 + 3)/2 * 10 = ?");
+  console.log("Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?");
+  console.log("What is the minimum crew size for the ISS?");
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-
+  if (question = true) {
+    console.log(correctAnswer);
+   } else {
+    console.log("Correct Answer:" + correctAnswer);
+   }
 
 
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
@@ -42,7 +49,7 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Greet candidate using their name //
-   console.log();
+   console.log("Hello," + candidateName);
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
